0f598a0ace771d9f80feb04044857796c1d2e7d5
Arthur <arthur.m.ramos@hotmail.com>
2024-03-03 12:51:42 -0300

    feat: add insta storie avatar effect

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -2,8 +2,9 @@ import { Avatar, AvatarFallback } from "./ui/avatar";
 
 export const AvatarComponent = () => {
   return (
-    <Avatar className="size-36">
+    <Avatar className="size-36 bg-gradient-to-tr from-fuchsia-500 via-violet-600 to-cyan-500 ">
       <img
+        className="rounded-full m-1 "
         src="https://avatars.githubusercontent.com/u/66889261?v=4"
         alt="Avatar"
       />
