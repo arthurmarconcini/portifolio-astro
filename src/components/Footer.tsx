8cfe8d4e0b3b05b0e6029a5c8bcfc9621e3fd57d
Arthur <arthur.m.ramos@hotmail.com>
2024-03-05 21:29:16 -0300

    Feat: Create a footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import { Card } from "./ui/card";
+
+export const Footer = () => {
+  return (
+    <Card className="p-4 rounded-none border-x-0">
+      <p className="font-light text-lg antialiased text-slate-700 text-center">
+        Desenvolvido por @arthurmarconcini
+      </p>
+    </Card>
+  );
+};
