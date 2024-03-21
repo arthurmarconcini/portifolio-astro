c7c02609212217e9cf0f62d0b1758747fb147529
Arthur <arthur.m.ramos@hotmail.com>
2024-03-20 21:01:35 -0300

    feat: add netlify conf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,20 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  })],
   image: {
     domains: ["avatars.githubusercontent.com", "github.com", "letsenhance.io"],
-    remotePatterns: [{ protocol: "https" }],
+    remotePatterns: [{
+      protocol: "https"
+    }]
   },
+  output: "server",
+  adapter: netlify()
 });
