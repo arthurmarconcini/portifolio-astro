cf1c9596a70889bf79e72bdbf54f52268a81850d
Arthur <arthur.m.ramos@hotmail.com>
2024-04-18 20:05:33 -0300

    chore: Remove ContactForm import

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { useForm } from "react-hook-form";
+import { Card, CardContent, CardTitle } from "./ui/card";
+import { Textarea } from "./ui/textarea";
+import { Button } from "./ui/button";
+
+const formSchema = z.object({
+  celphone: z.string().min(10).max(11),
+  email: z.string().email(),
+  text: z.string().min(10).max(100),
+});
+
+const ContactForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      celphone: "",
+      email: "",
+      text: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <Card className="w-80 md:w-96 lg:w-[32rem]">
+        <CardContent className="pt-6">
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col space-y-4"
+          >
+            <div className="flex space-x-4">
+              <FormField
+                control={form.control}
+                name="celphone"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Telefone</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Telefone" {...field} />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input type="email" placeholder="Email" {...field} />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <FormField
+              control={form.control}
+              name="text"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Mensagem</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Digite sua mensagem"
+                      className="h-20"
+                      {...field}
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" variant={"secondary"}>
+              Send
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </Form>
+  );
+};
+
+export default ContactForm;
