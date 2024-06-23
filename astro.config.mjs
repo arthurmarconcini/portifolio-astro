49796377f209b6dd52435b95ecb9438291a1ad7d
Arthur <arthur.m.ramos@hotmail.com>
2024-06-23 18:33:06 -0300

    feta: add server with vercel adapter.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
-  adapter: netlify(),
+  output: "server",
+  adapter: vercel({ imageService: true, devImageService: "squoosh" }),
   integrations: [
     react(),
     tailwind({
