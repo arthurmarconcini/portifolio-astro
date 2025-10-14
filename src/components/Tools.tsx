c8544e81c57aec5267c183c933a4c3404f0c2667
Arthur Marconcini Ramos <arthur.m.ramos@hotmail.com>
2025-10-14 18:02:56 -0300

    feat: add tools to tools components.

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -6,24 +6,20 @@ import jsImg from "./../assets/js.svg";
 import nextImg from "./../assets/next.svg";
 import tailwindImg from "./../assets/tailwind.svg";
 import nodeImg from "./../assets/node-js.svg";
+import typescriptImg from "./../assets/typescript.png";
+import prismaImg from "./../assets/prisma.svg";
 
 const tools = [
   { src: htmlImg.src, alt: "HTML" },
   { src: cssImg.src, alt: "CSS" },
   { src: jsImg.src, alt: "JavaScript" },
-  { name: "TypeScript" },
+  { src: typescriptImg.src, alt: "Typescript" },
   { src: reactImg.src, alt: "React" },
   { src: nextImg.src, alt: "Next.js", isRounded: true },
   { src: nodeImg.src, alt: "Node.js" },
-  { name: "AdonisJS" },
+  { src: prismaImg.src, alt: "Prisma" },
   { src: astroImg.src, alt: "Astro" },
   { src: tailwindImg.src, alt: "Tailwind CSS" },
-  { name: "Prisma" },
-  { name: "Java" },
-  { name: "C#" },
-  { name: "Dart" },
-  { name: "Flutter" },
-  { name: "Angular" },
 ];
 
 const Tool = ({ tool }: { tool: any }) => {
@@ -51,7 +47,9 @@ const Tool = ({ tool }: { tool: any }) => {
 const Tools = () => {
   return (
     <div className="w-full">
-      <h2 className="text-xl font-semibold text-center mb-4">Ferramentas e Tecnologias</h2>
+      <h2 className="text-xl font-semibold text-center mb-4">
+        Ferramentas e Tecnologias
+      </h2>
       <div className="flex flex-wrap justify-center gap-4">
         {tools.map((tool, index) => (
           <Tool key={index} tool={tool} />
